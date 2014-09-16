ee2924802765d47e4aa7924f13689a1cd6d53d21
App Academy Student <student@appacademy.io>
2014-09-16 14:32:10 -0700

    build entry model/collection

--- app/assets/javascripts/collections/entries_collection.js
+++ app/assets/javascripts/collections/entries_collection.js
@@ -1,6 +1,12 @@
 /*global NewsReader*/
 
 NewsReader.Collections.Entries = Backbone.Collection.extend({
+  initialize: function (options) {
+    this.feed = options.feed;
+  },
+  
   model: NewsReader.Models.Entry,
-  url: "api/entries"
+  url: function () {
+    return this.feed.url() + "/entries";
+  }
 });
